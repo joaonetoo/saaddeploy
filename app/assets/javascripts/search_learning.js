4e2ab12ebcba4eba2da4992e8ffb9a84fb07eefe
JulianSansat <kerarchie@yahoo.com.ar>
2016-09-21 15:21:10 -0300

    teachers restrition

--- app/assets/javascripts/search_learning.js
+++ app/assets/javascripts/search_learning.js
@@ -1,5 +1,7 @@
 $(document).ready(function () {
 
+    $('#_classroom_id').empty()
+    $('#_users_id').empty()
     $('#_subject_id').prepend("<option value='todos'>Todas</option>")
     $('#_subject_id option:first').attr("selected", "selected");
     $('#_classroom_id').prepend("<option value='todos'>Todas</option>")
@@ -8,6 +10,14 @@ $(document).ready(function () {
     $('#_users_id option:first').attr("selected", "selected");
 
     $('#_subject_id').change(function () {
+      if($('#_subject_id').val() == 'todos'){
+        $('#_classroom_id').empty()
+        $('#_classroom_id').prepend("<option value='todos'>Todas</option>")
+        $('#_classroom_id option:first').attr("selected", "selected");
+        $('#_users_id').empty()
+        $('#_users_id').prepend("<option value='todos'>Todos</option>")
+        $('#_users_id option:first').attr("selected", "selected");
+      }
       $.ajax( {
         url: "subject_selection",
         type: "GET",
@@ -16,6 +26,11 @@ $(document).ready(function () {
     })
 
     $('#_classroom_id').change(function () {
+      if($('#_classroom_id').val() == 'todos') {
+        $('#_users_id').empty()
+        $('#_users_id').prepend("<option value='todos'>Todos</option>")
+        $('#_users_id option:first').attr("selected", "selected");
+      }
       $.ajax( {
         url: "classroom_selection",
         type: "GET",
