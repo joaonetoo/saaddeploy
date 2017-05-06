da7cbc2c2cd4c87cec807593beb710fb9be71a10
joao <joaoantonioneto@outlook.com>
2017-05-05 23:46:34 -0300

    mudando a url da aplicacao

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,7 @@
 # Change these
 server '138.68.18.111', port: 22, roles: [:web, :app, :db], primary: true
 
-set :repo_url, 'https://github.com/JulianSansat/saaddeploy.git'
+set :repo_url, 'https://github.com/joaonetoo/saaddeploy.git'
 set :application, 'saad-v3'
 set :user, 'deploy'
 set :puma_threads,    [4, 16]
