86caa3625dade4951ccd17cab5618b60880b793c
joao <joaoantonioneto@outlook.com>
2017-05-15 02:04:27 -0300

    melhorando o  layout  do administrador

--- app/controllers/teachers_controller.rb
+++ app/controllers/teachers_controller.rb
@@ -69,6 +69,6 @@ class TeachersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def teacher_params
-      params.require(:teacher).permit(:nome, :telefone, :endereco, :lattes, :biografia, :matricula, :email)
+      params.require(:teacher).permit(:nome, :telefone, :endereco, :lattes, :biografia, :matricula, :email, :avatar)
     end
 end
